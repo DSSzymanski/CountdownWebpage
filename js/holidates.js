17280e658a4c55e8de045690aac507481da06ad8
dsszymanski <dan1993us@yahoo.com>
2021-09-14 14:31:31 -0400

    Fixed errors in holiday times & updated labor day

--- js/holidates.js
+++ js/holidates.js
@@ -1,12 +1,12 @@
 let holidays = [
-	["Labor Day", new Date('September 5, 2021 00:00:00'), new Date('September 6, 2021 23:00:00')],
 	["Thanksgiving", new Date('November 25 2021, 00:00:00'), new Date('November 25, 2021 23:59:59')],
 	["Christmas Eve", new Date('December 24, 2021 00:00:00'), new Date('December 24, 2021 23:59:59')],
-	["Christmas", new Date('December 24, 2021 17:00:00'), new Date('December 25, 2021 23:59:59')],
+	["Christmas", new Date('December 25, 2021 00:00:00'), new Date('December 25, 2021 23:59:59')],
 	["New Year/'s Eve", new Date('December 31, 2021 00:00:00'), new Date('December 31, 2021 23:59:59')],
 	["New Year/'s Day", new Date('January 1, 2022 00:00:00'), new Date('January 1, 2022 23:59:59')],
 	["Memorial Day", new Date('May 30, 2022 00:00:00'), new Date('May 30, 2022 23:59:59')],
-	["Independence Day", new Date('July 4, 2022 00:00:00'), new Date('July 4, 2022 23:59:59')]
+	["Independence Day", new Date('July 4, 2022 00:00:00'), new Date('July 4, 2022 23:59:59')],
+	["Labor Day", new Date('September 5, 2022 00:00:00'), new Date('September 5, 2021 23:59:59')]
 ];
 
 let isHoliday = (currDate) => {
