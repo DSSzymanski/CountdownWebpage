22bb9918959ba9a9d077aa71f0bfa00beb55fa07
dsszymanski <dan1993us@yahoo.com>
2021-09-14 14:32:34 -0400

    Added images to project. Updated background component to show all images associated with their holiday. Added author and link details to background component.

--- js/components/Background/Background.js
+++ js/components/Background/Background.js
@@ -1,33 +1,43 @@
 let Background = () => {
+	let background_img, background_author, background_link;
 	let date = new Date(Date.now());
 	let hol = getNextHol(date);
-	background_img = getImage(hol[0]);
+	[background_img, background_author, background_link] = getImage(hol[0]);
 
 	return (
 		React.createElement('div', {className:'image-container'},
-			React.createElement('img', {src:background_img, alt:hol[0]}, null)
+			React.createElement('a', {href: background_link}, 
+				React.createElement('img', {
+					src:background_img, 
+					alt:hol[0], 
+					title:String("See " + background_author + "'s picture at " + background_link)
+				}, null),
+			)
 		)
 	);
 }
 
 let getImage = (holiday) => {
 	if(holiday == "Labor Day"){
-		return;
+		return ['../images/labor-day.jpg', 'Philip Swinburn', 'https://unsplash.com/photos/vS7LVkPyXJU'];
 	}
 	else if(holiday == 'Thanksgiving') {
-		return '../images/thanksgiving.jpg';
+		return ['../images/thanksgiving.jpg', 'Claudio Schwarz', 'https://unsplash.com/photos/fXxsNyiqTio'];
 	}
 	else if(holiday == "Christmas Eve" || holiday == "Christmas") {
-		return;
+		return ['../images/christmas.jpg', 'Annie Spratt', 'https://unsplash.com/photos/QKo-op_gR9I'];
 	}
 	else if(holiday == "New Year/'s Eve" || holiday == "New Year/'s Day") {
-		return;
+		return ['../images/new-years.jpg', 'Cristian Escobar', 'https://unsplash.com/photos/abkEAOjnY0s'];
 	}
 	else if(holiday == "Memorial Day") {
-		return;
+		return ['../images/memorial-day.jpg', 'Chad Madden', 'https://unsplash.com/photos/IvD-eMV8PoU'];
 	}
 	else if(holiday == "Independence Day") {
-		return;
+		return ['../images/independence-day.jpg', 'Tom Dahm', 'https://unsplash.com/photos/oQGfLpwN22g'];
+	}
+	else{
+		return ['../images/labor-day.jpg', 'Philip Swinburn', 'https://unsplash.com/photos/vS7LVkPyXJU'];
 	}
 }
 
