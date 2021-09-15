1475006dd12738b8aa6025f3521a5f35f2d03ef5
dsszymanski <dan1993us@yahoo.com>
2021-09-15 14:36:36 -0400

    Changed getImage function to act as a lookup table instead of a if/else mess. Added comments to functions.

--- js/components/Background/Background.js
+++ js/components/Background/Background.js
@@ -1,15 +1,30 @@
+/**
+ * Background is a React component that returns the structure for the background
+ * image (div > a > img). The image will be clickable, which will bring the
+ * user to the owner's unsplash page where the image was obtained from. Also
+ * includes a hover displaying the author's name and the link as well.
+ * 
+ * @returns		{component}		returns a react component representing the background image.
+ */
 let Background = () => {
+	//vars for background information
 	let background_img, background_author, background_link;
 	let date = new Date(Date.now());
 	let hol = getNextHol(date);
+	//initializes vars to correct holiday image
 	[background_img, background_author, background_link] = getImage(hol[0]);
 
+	//return empty div if not nothing was returned
+	if(background_img == ""){return React.createElement('div', {className:'image-container'}, null);}
+
+	//return the component
 	return (
 		React.createElement('div', {className:'image-container'},
 			React.createElement('a', {href: background_link}, 
 				React.createElement('img', {
 					src:background_img, 
 					alt:hol[0], 
+					//hover text
 					title:String("See " + background_author + "'s picture at " + background_link)
 				}, null),
 			)
@@ -17,30 +32,31 @@ let Background = () => {
 	);
 }
 
+/**
+ * getImage is a function that acts as a basic lookup table. Returns holiday information if 
+ * it exists in table, otherwise returns array of 3 empty strings if not found.
+ * 
+ * @param	{string} 	holiday: string representing which holiday it is.
+ * 
+ * @returns	{array}		returns array of information in 3 strings about holiday in form 
+ * 							[image_path, image_author, image_url]
+ */
 let getImage = (holiday) => {
-	if(holiday == "Labor Day"){
-		return ['../images/labor-day.jpg', 'Philip Swinburn', 'https://unsplash.com/photos/vS7LVkPyXJU'];
-	}
-	else if(holiday == 'Thanksgiving') {
-		return ['../images/thanksgiving.jpg', 'Claudio Schwarz', 'https://unsplash.com/photos/fXxsNyiqTio'];
-	}
-	else if(holiday == "Christmas Eve" || holiday == "Christmas") {
-		return ['../images/christmas.jpg', 'Annie Spratt', 'https://unsplash.com/photos/QKo-op_gR9I'];
-	}
-	else if(holiday == "New Year/'s Eve" || holiday == "New Year/'s Day") {
-		return ['../images/new-years.jpg', 'Cristian Escobar', 'https://unsplash.com/photos/abkEAOjnY0s'];
-	}
-	else if(holiday == "Memorial Day") {
-		return ['../images/memorial-day.jpg', 'Chad Madden', 'https://unsplash.com/photos/IvD-eMV8PoU'];
-	}
-	else if(holiday == "Independence Day") {
-		return ['../images/independence-day.jpg', 'Tom Dahm', 'https://unsplash.com/photos/oQGfLpwN22g'];
-	}
-	else{
-		return ['../images/labor-day.jpg', 'Philip Swinburn', 'https://unsplash.com/photos/vS7LVkPyXJU'];
-	}
+	let holidayDict = {
+		"Labor Day": ['../images/labor-day.jpg', 'Philip Swinburn', 'https://unsplash.com/photos/vS7LVkPyXJU'],
+		"Thanksgiving": ['../images/thanksgiving.jpg', 'Claudio Schwarz', 'https://unsplash.com/photos/fXxsNyiqTio'],
+		"Christmas Eve": ['../images/christmas.jpg', 'Annie Spratt', 'https://unsplash.com/photos/QKo-op_gR9I'],
+		"Christmas": ['../images/christmas.jpg', 'Annie Spratt', 'https://unsplash.com/photos/QKo-op_gR9I'],
+		"New Year/'s Eve": ['../images/new-years.jpg', 'Cristian Escobar', 'https://unsplash.com/photos/abkEAOjnY0s'],
+		"New Year/'s Day": ['../images/new-years.jpg', 'Cristian Escobar', 'https://unsplash.com/photos/abkEAOjnY0s'],
+		"Memorial Day": ['../images/memorial-day.jpg', 'Chad Madden', 'https://unsplash.com/photos/IvD-eMV8PoU'],
+		"Independence Day": ['../images/independence-day.jpg', 'Tom Dahm', 'https://unsplash.com/photos/oQGfLpwN22g']
+	};
+
+	return (holiday in holidayDict ? holidayDict[holiday] : ['', '', '']);
 }
 
+//render image to page
 ReactDOM.render(
 	React.createElement(Background, {}, null),
 	document.getElementById('background')
