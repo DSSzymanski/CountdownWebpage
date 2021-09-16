13fd9b68b0e53bb1ec2bfefb794420176dc7311c
dsszymanski <dan1993us@yahoo.com>
2021-09-16 13:55:05 -0400

    Documented Banner component file

--- js/components/Banner/Banner.js
+++ js/components/Banner/Banner.js
@@ -1,15 +1,27 @@
+/**
+ * Banner is a React Component displaying the string header identifying which
+ * date it is/is coming up next. Returns a basic div with a string inner text
+ * with the holiday name and whether the timer represents the time until the
+ * holiday starts or time until the holiday ends if it currently is a holiday.
+ * 
+ * @returns 	{component} 	returns React component to be used as the 
+ * 								heading.
+ */
 let Banner = () => {
 	let date = new Date(Date.now());
 	let status = isHoliday(date);
 	let holString = getNextHol(date)[0];
 	let bannerString;
 
+	//Insert merry if it currently is Christmas or Christmas Eve
 	if(status == true && (holString == "Christmas" || holString == "Christmas Eve")) {
 		bannerString = 'Merry ';
 	}
+	//Insert Happy if it is currently a holiday not Xmas related
 	else if(status == true) {
 		bannerString = 'Happy ';
 	}
+	//Basic if the timer is representing a holiday coming up
 	else {
 		bannerString = 'Days until ';
 	}
