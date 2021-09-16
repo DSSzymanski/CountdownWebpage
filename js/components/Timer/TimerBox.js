8662b840e6c6317adfbecff87d9c7097935e4779
dsszymanski <dan1993us@yahoo.com>
2021-09-16 14:03:40 -0400

    Documented TimerBox.

--- js/components/Timer/TimerBox.js
+++ js/components/Timer/TimerBox.js
@@ -1,8 +1,18 @@
+/**
+ * TimerBox is a React component used to show individual timer value box (e.g. Hours,
+ * minutes, or seconds). Returns nested div, with a inner text string on the inside.
+ * 
+ * @param	{String} 	props.label: String to be displayed identifying which timer value
+ *			 						 the time is representing.
+ * @param	{Integer}	props.value: Integer representing a time value.
+ * 
+ * @returns	{component}	returns React component for a single timer value.
+ */	
 let TimerBox = (props) => {
 	return (
 		React.createElement('div', {className:'timer-box'},
 				React.createElement('div', {id: props.label}, String(props.value)),
-				String(props.label)
+				props.label
 		)
 	);
 }
